4734a9426e839169e4d336f5a3c8713cd77b184e
deepakbagati <deepakbagati2708@gmail.com>
2024-02-11 23:21:06 +0530

    files are config

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -11,7 +11,7 @@ const asyncHandler=(fn)=>async(req,res,next)=>{
     try{
        return  await fn(req,res,next)
     }
-    catch(error){
+    catch(err){
         res.status(err.code || 500).json({
             success:false,
             message:err.message
